d5fda7b79113560485b681c489c94edf4bcdb782
aylinoropeza <aylinoropeza.ao@gmail.com>
2025-03-25 07:46:57 -0400

    first commit

---
+++ src/utils/navigation.ts
@@ -0,0 +1,21 @@
+const navBarLinks = [
+    { name: "Inicio", url: "/" },
+    { name: "Sobre nosotros", url: "/about" },
+    { name: "Programas", url: "/programas" },
+    { name: "Donaciones", url: "/donaciones" },
+];
+
+const footerLinks = [
+    { icon: "facebook", label: "Fundación DESSOL", url: "https://www.facebook.com/profile.php?id=61567779628630" },
+    { icon: "gmail", label: "fundaciondessol.2025@gmail.com" },
+    { icon: "whatsapp", label: "+591 67640385", url: "https://wa.me/59167640385" },
+    {
+        icon: "location",
+        label: "Sucre - Bolivia, Calle Pasaje Peatonal S/N, Barrio Bajo Tucsupaya",
+    },
+]
+
+export default {
+    navBarLinks,
+    footerLinks,
+};
